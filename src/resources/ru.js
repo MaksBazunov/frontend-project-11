c39c1e77b2c29ac1a9abb790639c338920bea4b4
maksbazunov <maksbazunov@yandex.ru>
2023-05-05 00:15:41 +0300

    fix bug

--- src/resources/ru.js
+++ src/resources/ru.js
@@ -5,7 +5,8 @@ export default {
         isNotURL: 'Ссылка должна быть валидным URL',
         urlInAddedResources: 'RSS уже существует',
         isNotRssURL: 'Ресурс не содержит валидный RSS',
-        isValid: 'RSS успешно загружен',
+        isValid: 'RSS загружается',
+        isLoaded: 'RSS успешно загружен',
       },
     },
   };

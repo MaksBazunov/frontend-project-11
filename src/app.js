59f105fdf8f89dda451c77b2f071e300ff9ef580
maksbazunov <maksbazunov@yandex.ru>
2023-05-22 10:03:34 +0300

    4

--- src/app.js
+++ src/app.js
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
-/* eslint-disable no-undef */
+
 import { watcherValidation, watcherFillingDataForRss } from './view/watchers.js';
 import handlerButton from './handlers/handlerButton.js';
 import handlerInput from './handlers/handlerInput.js';
-=======
-/* eslint-disable max-len */
-import handlerOfBtnFormSection from './handlers/btnFormSectionHandler.js';
-import { handlerOfLinkOpeningBtn } from './handlers/modalHandlers.js';
-import {
-  watcherValidationRssURL, watcherActivityButton,
-  watcherLoadingRssContent,
-} from './view/watchers.js';
->>>>>>> 9f3492a2056b0efa759a49ff0bc5ff751bfe669f
 
 const app = (state) => {
   const input = document.querySelector('#url-input');
--- src/validators/validatorForm.js
+++ src/validators/validatorForm.js
@@ -2,20 +2,22 @@ import * as yup from 'yup';
 import { setLocale } from 'yup';
 
 const validateForm = (i18n, content) => {
-  console.log(content, 'content');
   setLocale({
     mixed: {
       default: 'field_invalid',
     },
     string: {
-      url: i18n.t('validation.errors.errorURL'),
-      min: i18n.t('validation.errors.errorRequared'),
+      url: i18n.t('isNotURL'),
+      required: i18n.t('isNotURL'),
     },
   });
-
-  const shema = yup.string().url().min(1);
-
-  return shema.validate(content);
+  const shema = yup.object().shape({
+    rssUrl: yup.string()
+      .required()
+      .url()
+      .trim(),
+  });
+  return shema.validate({ rssUrl: content });
 };
 
 export default validateForm;

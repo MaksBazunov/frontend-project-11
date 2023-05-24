54bafe57096b7cb5202afb3e1010a3fa95f854dc
maksbazunov <maksbazunov@yandex.ru>
2023-05-24 23:37:51 +0300

    creat topic

--- src/handlers/handlerDataRSSPosts.js
+++ src/handlers/handlerDataRSSPosts.js
@@ -1,7 +1,23 @@
-const handlerDataRSSPosts = (watcherFillingDataForRss, resultOfValidation) => {
-  watcherFillingDataForRss.process = 'loadingRss';
-  // eslint-disable-next-line max-len
-  watcherFillingDataForRss.resources.push({ id: watcherFillingDataForRss.feeds.length, value: resultOfValidation });
+import axios from 'axios';
+import parserRSS from '../parsers/parserRss.js';
+
+const handlerDataRSSPosts = (watcherFillingDataForRSS, resultOfValidation) => {
+  watcherFillingDataForRSS.process = 'loadingRss';
+
+  const id = watcherFillingDataForRSS.resources.length;
+  watcherFillingDataForRSS.resources.push({ id, value: resultOfValidation });
+
+  const proxy = 'https://allorigins.hexlet.app/get?';
+  axios.get(`${proxy}disableCache=true&url=${encodeURIComponent(resultOfValidation)}/`)
+    .then((response) => parserRSS(response, id))
+    .then((parsedRss) => {
+      const { feed, topic } = parsedRss;
+      watcherFillingDataForRSS.feeds.push(feed);
+      watcherFillingDataForRSS.topics.push(topic);
+      watcherFillingDataForRSS.process = 'renderingRSS';
+      watcherFillingDataForRSS.process = 'loadingRSS';
+    })
+    .catch((e) => console.log(e));
 };
 
 export default handlerDataRSSPosts;
--- src/parsers/parserRss.js
+++ src/parsers/parserRss.js
@@ -1,30 +1,19 @@
-import { getUniqId } from '../handlers/additionalHandlers.js';
-
 const parserRSS = (response, id) => {
-  try {
-    const parser = new DOMParser();
-    const data = parser.parseFromString(response.data.contents, 'text/xml');
-    const feed = {
-      title: data.querySelector('channel title').textContent,
-      description: data.querySelector('channel description').textContent,
-      id,
-    };
-
-    const topics = Array.from(data.querySelectorAll('item')).map((item) => {
-      const top = {
-        title: item.querySelector('title').textContent,
-        link: item.querySelector('link').textContent,
-        description: item.querySelector('description').textContent,
-        id,
-        childrenId: `#i${getUniqId()}`,
-      };
-      return top;
-    });
-
-    return { feed, topics };
-  } catch {
-    throw new Error('errorParsing');
-  }
+  const parser = new DOMParser();
+  const data = parser.parseFromString(response.data.contents, 'text/xml');
+  const feed = {
+    tittle: data.querySelector('channel').children[0].textContent,
+    description: data.querySelector('channel').children[1].textContent,
+    id,
+  };
+  const topic = {
+    tittle: data.querySelector('channel item').children[0].textContent,
+    link: data.querySelector('channel item').children[2].textContent,
+    description: data.querySelector('channel item').children[3].textContent,
+    id,
+  };
+  console.log(topic.link);
+  return { feed, topic };
 };
 
 export default parserRSS;

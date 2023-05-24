2f0f843d15fce58bc4fb20e599c1f4f9daa6b5bd
maksbazunov <maksbazunov@yandex.ru>
2023-05-24 23:02:32 +0300

    fix bug module

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 
 import * as path from 'path';
-// import { fileURLToPath } from 'url';
-// import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+ const __dirname = dirname(__filename);
 
 export default {
   mode: 'development',

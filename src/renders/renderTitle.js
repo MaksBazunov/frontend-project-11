0c80372a7f03f6c11e695e7c245c8fec8e29767a
maksbazunov <maksbazunov@yandex.ru>
2023-05-26 00:57:25 +0300

    creat file 6

--- src/renders/renderTitle.js
+++ src/renders/renderTitle.js
@@ -1,13 +1,12 @@
 const renderTitle = (viewedTopics) => {
-    console.log(viewedTopics, '4444444444444444444');
-  viewedTopics.forEach((viewedTopicId) => {
-    const currentId = viewedTopicId;
-    const currentLi = document.querySelector(`[data-topic-id="${currentId}"]`);
-    console.log(currentId, currentLi, '5555555555555555555');
-    const a = currentLi.querySelector('a');
-    a.classList.remove('fw-bold');
-    a.classList.add('fw-normal', 'link-secondary');
-  });
-};
-
-export default renderTitle;
+    viewedTopics.forEach((viewedTopicId) => {
+      console.log(viewedTopicId);
+      const currentId = viewedTopicId;
+      const currentLi = document.querySelector(`[data-topic-childrenId="${currentId}"]`);
+      const a = currentLi.querySelector('a');
+      a.classList.remove('fw-bold');
+      a.classList.add('fw-normal', 'link-secondary');
+    });
+  };
+  
+  export default renderTitle;
